b487cd15b18186da94f8d41c77228b745cd09ce5
username <2426170744@qq.com>
2017-10-02 15:22:35 +0800

    1132. Cut Integer (20)

--- 1132(Cut Integer).cpp
+++ 1132(Cut Integer).cpp
@@ -1,4 +1,4 @@
-// 1032(Cut Integer).cpp : 定义控制台应用程序的入口点。
+// 1132(Cut Integer).cpp : 定义控制台应用程序的入口点。
 //
 
 #include "stdafx.h"
@@ -8,20 +8,26 @@ using namespace std;
 
 int main()
 {
-	string s;
-	long long int Z, A, B;
-	int N;
+	string s, s1, s2;
+	long long int N, Z,A,B;
 	cin >> N;
 	for (int i = 0; i < N; i++) {
 		cin >> s;
 		Z = stoll(s);
-		A = stoll(s.substr(0, s.size() / 2));
-		cout << s.substr(0, s.size() / 2) << endl;
-		B = stoll(s.substr(s.size() / 2+1, s.size()));
-		cout << s.substr(s.size() / 2 , s.size()-1) << endl;
-		printf("Z = %d , A = %d , B = %d\n",Z,A,B);
+		s1 = s.substr(0, s.size() / 2);
+		s2 = s.substr(s.size() / 2, s.size() - 1);
+		A = stoll(s1);
+		//cout << A<<" ";
+		B = stoll(s2);
+		//cout << B;
+		if (A*B == 0) { cout << "No" << endl; continue; }
+		if (Z%(A*B)==0) {
+			cout << "Yes" << endl;
+		}
+		else {
+			cout << "No" << endl;
+		}
 	}
-
     return 0;
 }
 
