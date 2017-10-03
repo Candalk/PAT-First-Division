2fbe2359624deb4f242b4840e010e3a89429b780
username <2426170744@qq.com>
2017-10-03 14:56:27 +0800

    1116. Come on! Let's C (20)

--- 1116(Come on! Let's C).cpp
+++ 1116(Come on! Let's C).cpp
@@ -9,15 +9,14 @@ using namespace std;
 bool isPrime(int k) {
 	if (k == 1)
 		return false;
-	for (int i = 2; i <= sqrt(k); i++) {
+	for (int i = 2; i <= sqrt(double(k)); i++) {
 		if (k%i == 0)return false;
 	}
 	return true;
 }
 int main()
 {
-	int N, rank[10002], k, checked[10002] = { 0 };
-	memset(rank, -1, 10000);
+	int N, rank[10002] = { 0 }, k, checked[10002] = { 0 };
 	cin >> N;
 	for (int i = 1; i <= N; i++) {
 		cin >> k;
@@ -26,13 +25,15 @@ int main()
 	cin >> N;
 	for (int i = 1; i <= N; i++) {
 		cin >> k;
-		if (rank[k] == -1)
+		if (rank[k] == 0){
 			printf("%04d: Are you kidding?\n", k);
-		else if (checked[k] == 1) {
-			printf("%04d: Checked\n", k);
 			continue;
 		}
-		else if (rank[k] == 1) {
+		if (checked[k] == 1) {
+			printf("%04d: Checked\n", k);
+			continue;
+		}else checked[k] = 1;
+		if (rank[k] == 1) {
 			printf("%04d: Mystery Award\n", k);
 		}
 		else if (isPrime(rank[k])) {
@@ -41,7 +42,7 @@ int main()
 		else if (rank[k] >= 1) {
 			printf("%04d: Chocolate\n", k);
 		}
-		checked[k] = 1;
+		
 	}
 	return 0;
 }
