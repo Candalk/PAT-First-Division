159bf5136612fa10a8fbb1f68aa1b0be1bc7d3ed
Candalk <2426170744@qq.com>
2017-10-29 23:39:12 +0800

    1125. Chain the Ropes (25)

--- 1125(Chain the Ropes).cpp
+++ 1125(Chain the Ropes).cpp
@@ -1,7 +1,3 @@
-// 1125(Chain the Ropes).cpp : 定义控制台应用程序的入口点。
-//
-
-#include "stdafx.h"
 #include<cstdio>
 #include<vector>
 #include<algorithm>
@@ -16,8 +12,8 @@ int main()
 		scanf("%d", &v[i]);
 	}
 	sort(v.begin(), v.end());
-	double sum = 0;
-	for (int i = 0; i < N; i++) {
+	double sum = v[0];
+	for (int i = 1; i < N; i++) {
 		sum = (sum + v[i])*1.0 / 2;
 	}
 	
